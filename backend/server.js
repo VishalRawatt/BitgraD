d0269278a9ebcdf539c263daf50fcfbe7a28a44b
Vishal Rawat <94183896+VishalRawatt@users.noreply.github.com>
2023-10-02 19:52:32 +0530

    cors delete

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,16 @@ const db = require('./db')
 const router = require('./routes/index')
 
 //cors
-
 app.use(bodyParser.json({limit:"50mb"}))
 app.use(bodyParser.urlencoded({extended:true, limit:"50mb"}))
 
+app.use((req, res,next) => {
+    res.header('Access-Control-Allow-Origin', 'https://bitgrad.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
-app.use(cors())
 //routes
 
 app.use('/', router) ;
