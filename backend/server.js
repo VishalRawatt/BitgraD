524d2e9d0e265de42d84d23729b14cc7aaaf819e
Vishal Rawat <94183896+VishalRawatt@users.noreply.github.com>
2023-10-02 19:44:17 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,12 @@ const db = require('./db')
 const router = require('./routes/index')
 
 //cors
+app.use(cors())
 app.use(bodyParser.json({limit:"50mb"}))
 app.use(bodyParser.urlencoded({extended:true, limit:"50mb"}))
 
 app.use((req, res,next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://bitgrad.vercel.app/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
