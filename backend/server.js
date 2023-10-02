42ccfc7ef1bafbe8e2a212832da27bf1b5204b53
Vishal Rawat <94183896+VishalRawatt@users.noreply.github.com>
2023-10-02 19:56:06 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -12,15 +12,9 @@ const router = require('./routes/index')
 app.use(bodyParser.json({limit:"50mb"}))
 app.use(bodyParser.urlencoded({extended:true, limit:"50mb"}))
 
-app.use((req, res,next) => {
-    res.header('Access-Control-Allow-Origin', 'https://bitgrad.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
 
 //routes
-
+app.use(cors())
 app.use('/', router) ;
 
 // app.use('/uploads', express.static(path.join(__dirname, "../uploads")))
@@ -35,7 +29,7 @@ app.use('/', router) ;
 //     }
 // });
 
-app.use(cors())
+
 db.connect();
 
 //server listen 
